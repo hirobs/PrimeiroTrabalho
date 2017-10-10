c437fc83bbdf50793bb4e5bd77385bb21750463b
hirobs <hiro.bs@outlook.com>
2017-10-09 22:06:13 -0300

    Terminado

--- app/src/main/java/com/example/hirob/primeirotrabalho/MainActivity.java
+++ app/src/main/java/com/example/hirob/primeirotrabalho/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button botaoTela;
-    private Button
+    private Button botaoLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
 
         botaoTela = (Button) findViewById(R.id.buttonCadastrar);
 
+        botaoLista = (Button) findViewById(R.id.buttonListar);
+
         botaoTela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
+        botaoLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent it = new Intent(MainActivity.this, Main3Activity.class);
+                it.putExtra("bandeira", "NAO_TEM_NADA");
+                startActivity(it);
+               // startActivity(new Intent(MainActivity.this, Main3Activity.class));
             }
         });
     }
